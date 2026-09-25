67bfe21d4b25315e14717a90763aee1c2e2b9043
goutam23511 <goutam.prithvi@gmail.com>
2026-09-25 10:48:26 +0530

    24 hour time to 12 hour time

---
+++ main/24hrs_12hrs.py
@@ -0,0 +1,13 @@
+
+Time=input("Enter the time: ")
+split=Time.split(":")
+
+if int(split[0])<=12:
+    split[0]=split[0]
+elif int(split[0])>24:
+    print("Dont play around")
+    exit()
+else:
+    split[0]=int(split[0])-12
+
+print(f'{split[0]}:{split[1]}')
